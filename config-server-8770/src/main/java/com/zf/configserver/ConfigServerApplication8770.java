b1e663158eff5bbf8cbc4efdf7dc723da3d113ce
allenzhangfan <allenzhangfan@gmail.com>
2020-06-02 17:18:53 +0800

    zf

---
+++ config-server-8770/src/main/java/com/zf/configserver/ConfigServerApplication8770.java
@@ -0,0 +1,15 @@
+package com.zf.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication8770 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication8770.class, args);
+    }
+
+}
---
+++ config-server-8770/src/test/java/com/zf/configserver/ConfigServerApplication8770Tests.java
@@ -0,0 +1,13 @@
+package com.zf.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplication8770Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
